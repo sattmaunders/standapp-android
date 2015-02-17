e582569c2feaefb08e87e9e2118044aff6e3387d
mmmyum <matt@mattsaunders.info>
2015-02-17 17:06:25 -0500

    Change action bar colour on tab switch

--- mobile/src/main/java/com/standapp/activity/MainActivity.java
+++ mobile/src/main/java/com/standapp/activity/MainActivity.java
@@ -3,7 +3,6 @@ package com.standapp.activity;
 import android.accounts.AccountManager;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
